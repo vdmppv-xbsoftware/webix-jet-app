3590239cb5b35b124707ec53a7a35f9d68a29bb8
vdmppv-xbsoftware <102971428+vdmppv-xbsoftware@users.noreply.github.com>
2022-05-08 20:22:33 +0300

    settings page executed

---
+++ sources/models/icons.js
@@ -0,0 +1,76 @@
+const icons = new webix.DataCollection({
+	data: [
+		{
+			id: "flag",
+			value: "",
+			icon: "webix_icon mdi mdi-flag"
+		},
+		{
+			id: "comment",
+			value: "",
+			icon: "webix_icon mdi mdi-comment"
+		},
+		{
+			id: "clock",
+			value: "",
+			icon: "webix_icon mdi mdi-clock"
+		},
+		{
+			id: "phone",
+			value: "",
+			icon: "webix_icon mdi mdi-phone"
+		},
+		{
+			id: "comment",
+			value: "",
+			icon: "webix_icon mdi mdi-comment"
+		},
+		{
+			id: "cogs",
+			value: "",
+			icon: "webix_icon mdi mdi-cogs"
+		},
+		{
+			id: "account",
+			value: "",
+			icon: "webix_icon mdi mdi-account"
+		},
+		{
+			id: "pencil",
+			value: "",
+			icon: "webix_icon mdi mdi-pencil"
+		},
+		{
+			id: "plus",
+			value: "",
+			icon: "webix_icon mdi mdi-plus"
+		},
+		{
+			id: "coffee",
+			value: "",
+			icon: "webix_icon mdi mdi-coffee"
+		},
+		{
+			id: "gamepad",
+			value: "",
+			icon: "webix_icon mdi mdi-gamepad-variant"
+		},
+		{
+			id: "gift",
+			value: "",
+			icon: "webix_icon mdi mdi-gift"
+		},
+		{
+			id: "food",
+			value: "",
+			icon: "webix_icon mdi mdi-food"
+		},
+		{
+			id: "car",
+			value: "",
+			icon: "webix_icon mdi mdi-car"
+		}
+	]
+});
+
+export default icons;
--- sources/views/settingsView.js
+++ sources/views/settingsView.js
@@ -1,50 +1,14 @@
 import {JetView} from "webix-jet";
 
-import SettingsPopup from "./settingsPopup";
+import activityTypesCollection from "../models/activitytypes";
+import statusesCollection from "../models/statuses";
+// import SettingsPopup from "./settingsPopup";
 import SettingsTable from "./settingsTable";
 
 export default class SettingsView extends JetView {
 	config() {
 		const locale = this.app.getService("locale");
 		const _ = locale._;
-
-		const addStatusHeader = {
-			cols: [
-				{
-					template: _("Statuses"),
-					type: "header"
-				},
-				{
-					width: 150,
-					view: "button",
-					css: "webix_primary",
-					type: "icon",
-					icon: "webix_icon wxi-plus",
-					label: _("Add new"),
-					align: "center",
-					click: () => this.windowStatus.showWindow()
-				}
-			]
-		};
-
-		const addTypeHeader = {
-			cols: [
-				{
-					template: _("Activity type"),
-					type: "header"
-				},
-				{
-					width: 150,
-					view: "button",
-					css: "webix_primary",
-					type: "icon",
-					icon: "webix_icon wxi-plus",
-					label: _("Add new"),
-					align: "center",
-					click: () => this.windowActivityType.showWindow()
-				}
-			]
-		};
 
 		return {
 			padding: 15,
@@ -68,25 +32,18 @@ export default class SettingsView extends JetView {
 					gravity: 0.1
 				},
 				{
+					margin: 20,
 					cols: [
 						{
-							rows: [addStatusHeader]
+							rows: [new SettingsTable(this.app, statusesCollection, _("Status"), _("Statuses"))]
 						},
 						{
-							gravity: 0.2
-						},
-						{
-							rows: [addTypeHeader]
+							rows: [new SettingsTable(this.app, activityTypesCollection, _("Activity"), _("Activity type"))]
 						}
 					]
 				},
 				{}
 			]
 		};
-	}
-
-	init() {
-		// this.statusPopup = this.ui(new SettingsPopup());
-		// this.typePopup = this.ui(new SettingsPopup());
 	}
 }
